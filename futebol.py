39419339bf96dd3dca6d0886a9c3b84d3623dc32
Gabriel <gabrielbianchin.gb@gmail.com>
2019-05-30 22:54:24 -0300

    Removido arestas proximas a jogadores do time adversario e lista de adj dos vertices

--- futebol.py
+++ futebol.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from scipy.spatial import Delaunay
+from igraph import *
 
 data_array = np.loadtxt('CapSpoT2Suav.2d').astype('int')
 
@@ -17,8 +18,6 @@ for i in range(num_players):
     players_pos_team1[:,i] = data_array[:, (2*i)+1:(2*i)+3]
     players_pos_team2[:,i] = data_array[:, (2*num_players-1)+(2*i):(2*num_players-1)+(2*i)+2]
 
-#fig = plt.figure()
-
 frame = 3000
 
 in_game_team1 = players_pos_team1[frame][np.all(players_pos_team1[frame] >= 0, axis=1)]
@@ -26,19 +25,55 @@ in_game_team2 = players_pos_team2[frame][np.all(players_pos_team2[frame] >= 0, a
 tri1 = Delaunay(in_game_team1)
 tri2 = Delaunay(in_game_team2)
 
-coord_groups = [tri1.points[x] for x in tri1.simplices]
+vertex_graphs = tri2.simplices
+coord = [tri2.points[x] for x in tri2.simplices]
 
-for conj_points in coord_groups:
-	p1 = conj_points[0]
-	p2 = conj_points[1]
-	p3 = conj_points[2]
-	for pointt in in_game_team2:
-		d1 = np.cross(abs(p2-p1),abs(pointt - p1))/np.linalg.norm(abs(p2 - p1))
-		d2 = np.cross(abs(p3-p2),abs(pointt - p2))/np.linalg.norm(abs(p3 - p2))
-		d3 = np.cross(abs(p1-p3),abs(pointt - p3))/np.linalg.norm(abs(p1 - p3))
-		if d1 < 1 or d2 < 1 or d3 < 1:
-			print(conj_points)
+final_points1 = []
+final_points2 = []
 
+g = Graph()
+
+g.add_vertices(12)
+
+for i in range(len(coord)):
+	coords = coord[i]
+	vertex = vertex_graphs[i]
+
+	v0 = vertex[0]
+	v1 = vertex[1]
+	v2 = vertex[2]
+
+	p0 = np.array(coords[0])
+	p1 = np.array(coords[1])
+	p2 = np.array(coords[2])
+
+	total_d0 = []
+	total_d1 = []
+	total_d2 = []
+
+	for points in in_game_team1:
+		d0 = abs((np.cross(p1-p0, p1-points)/np.linalg.norm(p1-p0)))
+		d1 = abs((np.cross(p2-p1, p2-points)/np.linalg.norm(p2-p1)))
+		d2 = abs((np.cross(p0-p2, p0-points)/np.linalg.norm(p0-p2)))
+		if d0 < 1.5:
+			total_d0.append(d0)
+		if d1 < 1.5:
+			total_d1.append(d1)
+		if d2 < 1.5:
+			total_d2.append(d2)
+
+	if not total_d0:
+		final_points1.append(p0)
+		final_points2.append(p1)
+		g.add_edge(v0, v1)
+	if not total_d1:
+		final_points1.append(p1)
+		final_points2.append(p2)
+		g.add_edge(v1, v2)
+	if not total_d2:
+		final_points1.append(p2)
+		final_points2.append(p0)
+		g.add_edge(v0, v2)
 
 plt.triplot(in_game_team1[:,0], in_game_team1[:,1], tri1.simplices.copy(), c='b')
 plt.triplot(in_game_team2[:,0], in_game_team2[:,1], tri2.simplices.copy(), c='r')
@@ -47,3 +82,14 @@ plt.ylim(0,60)
 plt.scatter(in_game_team1[:,0], in_game_team1[:,1], c='b')
 plt.scatter(in_game_team2[:,0], in_game_team2[:,1], c='r')
 plt.show()
+
+for i in range(len(final_points2)):
+	x1, y1 = final_points1[i]
+	x2, y2 = final_points2[i]
+	saida1 = [x1, x2]
+	saida2 = [y1, y2]
+	plt.plot(saida1, saida2, marker = 'o', c='r')
+plt.show()
+
+print(g)
+
